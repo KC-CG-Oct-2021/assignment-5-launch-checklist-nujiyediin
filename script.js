0045a082264168494e44281512f756916517bd58
Your Name <nujiyediin@gmail.com>
2022-03-07 01:44:46 -0600

    edited scripthelper.js and script.js, completed sections Updating Shuttle Requirements and Adding Alerts

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
+window.addEventListener("load", function() {
+    console.log("window loaded");
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        formSubmission(document, "", "input[name=pilotName]", "input[name=copilotName]", "input[name=fuelLevel]", "input[name=cargoMass]");
+        console.log("event");
+
+   })
+   
+   
+   
+    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
@@ -12,5 +24,11 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+
+
+   
+
+
+
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,9 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
+
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -17,11 +20,80 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === "") {
+       return "Empty";
+   } else if(isNaN(testInput)) {
+       return "Not a number";
+   } else {
+       return "Is a number";
+   }
+
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    let pilotName = document.querySelector(pilot);
+    let copilotName = document.querySelector(copilot);
+    let fuel = document.querySelector(fuelLevel);
+    let cargoMass = document.querySelector(cargoLevel);
+    
    
+
+
+    if (validateInput(pilotName.value) === "Empty" || validateInput(copilotName.value) === "Empty" || validateInput(fuel.value) === "Empty" || validateInput(cargoMass.value) === "Empty") {
+        alert("All fields are required.");
+    }
+
+   if (validateInput(pilotName.value) === "Is a number" || validateInput(copilotName.value) === "Is a number") {
+       alert("Names cannot be numbers.");
+   }
+
+   if (validateInput(cargoMass.value) === "Not a number") {
+       alert("You must enter a number for cargo mass.");
+   }
+
+   if (validateInput(fuel.value) === "Not a number") {
+       alert("You must enter a number for fuel level.");
+   }
+
+
+   console.log(document.querySelector("#pilotStatus"));
+
+    let pilotStatus = document.querySelector("#pilotStatus");
+    pilotStatus.innerHTML = "Pilot " + pilotName.value + " Ready";
+
+    let copilotStatus = document.querySelector("#copilotStatus");
+    copilotStatus.innerHTML = "Copilot " + copilotName.value + " Ready";
+
+    let faultyItems = document.querySelector("#faultyItems");
+
+    let fuelStatus = document.querySelector("#fuelStatus");
+
+    let launchStatus = document.querySelector("#launchStatus");
+
+    if (fuel.value < 10000) {
+        faultyItems.style.visibility = "visible";
+        fuelStatus.innerHTML = "There is not enough fuel for this journey.";
+        launchStatus.style.color = "red";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+    }
+    
+
+    let cargoStatus = document.querySelector("#cargoStatus");
+
+    if (cargoMass.value > 10000) {
+        faultyItems.style.visibility = "visible";
+        cargoStatus.innerHTML = "There is too much mass for the shuttle to take off.";
+        launchStatus.style.color = "red";
+        launchStatus.innerHTML = "Shuttle not ready for launch"
+    }
+
+    else {
+        launchStatus.innerHTML = "Shuttle is ready for launch.";
+        launchStatus.style.color = "green";
+    }
+   
+
+
 }
 
 async function myFetch() {
